1b54b2450225fda8691123aa0a0c4447c4cbb16a
root <libofan1995@163.com>
2024-02-01 16:56:47 +0800

    Updated code logic for writing the config file

--- sgpt_installer_for_PWD.sh
+++ sgpt_installer_for_PWD.sh
@@ -18,12 +18,14 @@ fi
 function spgt_api_key_setter(){
 echo -e "Please enter the OPENAI API key (Check with Leon if you don't have any)\n"
 read API_KEY
+line_number_for_Key=""
 if [[ -n $API_KEY ]]; then
 	if [[ ! -f root/.config/shell_gpt/.sgptrc ]]; then
 		mkdir -p /root/.config/shell_gpt
 		touch /root/.config/shell_gpt/.sgptrc
 	fi
-	sed -i "1s "OPENAI_API_KEY=${API_KEY}"" /root/.config/shell_gpt/.sgptrc
+	line_number_for_Key=$(awk '/OPENAI_API/{print NR}' /root/.config/shell_gpt/.sgptrc)
+	sed -i "${line_number_for_Key}c "OPENAI_API_KEY=${API_KEY}"" /root/.config/shell_gpt/.sgptrc
 	[[ $? -eq 0 ]] && echo "INFO: API key inserted." || echo "ERROR: API key insertion failed!"
 else
 	echo "ERROR: The API key cannot be empty string!"
