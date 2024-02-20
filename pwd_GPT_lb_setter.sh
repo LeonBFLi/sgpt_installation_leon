65edce9c220fd58e344a76c0294ef040b1e42a70
Leon B F Li <libofan1995@163.com>
2024-02-20 13:53:54 +0800

    Adding pwd env lb setter scirpt

---
+++ pwd_GPT_lb_setter.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#function: set up nginx lb for GPT access from a PWD instance
+
+#define vars
+prepared_img="andylbf/lb_pwd"
+
+#define functions
+function lb_container_starter(){
+echo "INFO: please open your port 80 for the access"
+docker pull $prepared_img && (docker run -itd --name lb -p 80:80 prepared_img)
+}
+
+
+###Main scirpt###
+lb_container_starter
+###Main scirpt###
