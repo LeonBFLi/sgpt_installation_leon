6ad450094ead47fe1e6e30ff1254146a0c28b00e
LeonBFLi <libofan1995@163.com>
2024-02-26 21:10:49 +0800

    update config file

--- pwd_proxy_builder.sh
+++ pwd_proxy_builder.sh
@@ -41,8 +41,6 @@ http {
         listen 80;
         location / {
             proxy_pass http://backend/;
-            proxy_set_header Host $host;
-            proxy_set_header X-Real-IP $remote_addr;
         }
     }
 }
