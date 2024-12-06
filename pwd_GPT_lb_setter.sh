ce199aebfd60b9a5dd98d13443c43af8b4bec439
Leon B F Li <88610487+LeonBFLi@users.noreply.github.com>
2024-12-06 17:28:46 +0800

    Create pwd_GPT_lb_setter.sh

---
+++ pwd_GPT_lb_setter.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#function: this script is used for setting up nginx lb in PWD playground env
+
+#vars
+nginx_config_file="/etc/nginx/nginx.conf"
+
+#functions
+packages_installer(){
+apk update && apk upgrade
+apk add nginx && apk add openrc --no-cache
+}
+
+rc_softlevel_enabler(){
+if [[ ! -f /run/openrc/softlevel  ]]; then
+touch /run/openrc/softlevel
+fi
+}
+
+nginx_conf_writter(){
+cat << EOF > $nginx_config_file
+error_log  /var/log/nginx/error.log notice;
+pid        /var/run/nginx.pid;
+
+events {
+    worker_connections  1024;
+}
+
+
+http {
+    access_log /var/log/nginx/access.log;
+
+    upstream backend {
+        server talkwithme.top;
+    }
+
+    server {
+        listen 80;
+        location / {
+            proxy_pass http://backend/;
+        }
+
+        client_max_body_size 999M;
+    }
+}
+EOF
+}
+
+function service_starter(){
+echo "INFO: $(nginx -t)"
+rc-service nginx restart
+echo "INFO: please access the page via port 80"
+}
+
+
+#main script
+packages_installer
+rc_softlevel_enabler
+nginx_conf_writter
+service_starter
+#main script
